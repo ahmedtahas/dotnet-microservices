934988903ad616bf9fe7ab5d4d026bb72c380ff8
Ahmed Taha Sarı <ahmed.t.sari@gmail.com>
2023-10-25 17:55:33 +0300

    added new tests

--- ProductApi.Tests/UserServiceTests.cs
+++ ProductApi.Tests/UserServiceTests.cs
@@ -1,31 +1,34 @@
-using NUnit.Framework;
+using Xunit;
 using ProductApi.Services;
 using Moq;
 using Microsoft.Extensions.Configuration;
-
 
 namespace ProductApi.Tests.Services
 {
     public class UserServiceTests
     {
-        [Test]
+        [Fact]
         public void Authenticate_UserDoesNotExist_ReturnsFalse()
         {
             var configMock = new Mock<IConfiguration>();
 
             var userService = new UserService(new AuthenticationService(configMock.Object));
             var result = userService.Authenticate("nonexistentUser", "password");
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [Test]
+        [Fact]
         public void Authenticate_UserExistsButSaltIsNull_ReturnsFalse()
         {
-           var configMock = new Mock<IConfiguration>();
+            var configMock = new Mock<IConfiguration>();
 
             var userService = new UserService(new AuthenticationService(configMock.Object));
             var user = userService.Register("existingUser", "password");
             user.Salt = null;
+
+            // You need to add an assertion here to check the expected behavior
+            // For example, if you expect an exception to be thrown when the salt is null, you can do:
+            // Assert.Throws<NullReferenceException>(() => userService.Authenticate("existingUser", "password"));
         }
     }
 }
